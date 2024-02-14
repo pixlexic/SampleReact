790335ea43612b4b5e0bb89cf3844b3d58dd6685
pixlexic <jones@pixlexic.com>
2024-02-14 11:36:02 -0500

    reload

---
+++ reactsample/src/app/models/sampleitemmodel.js
@@ -0,0 +1,14 @@
+
+export default class SampleItemModel {
+
+    id = null;
+    title = null;
+    description = null;
+    cost = 0;
+    qty = 10;
+    sold = 10;
+
+
+
+}
+
---
+++ reactsample/src/app/services/globalservice.js
@@ -0,0 +1,15 @@
+export default class GlobalService{
+
+
+    static appversion = '0.0.1';
+
+    static parentURL = 'https://localhost:7235';
+  
+  
+    constructor() { }
+
+
+
+}
+
+
